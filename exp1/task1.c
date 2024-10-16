4d36642965de4f085cef661296c2fa41ba53a184
MariyamKG <mariyam@eng.rizvi.edu.in>
2024-10-16 10:27:23 +0530

    Create task1.c

---
+++ exp1/task1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+printf("Name\t:Mariyam\n");
+printf("Age\t\t:18\n");
+printf("Class\t\t:FE\n");
+printf("Division\t:E\n");
+printf("UIN\t\t:241S048\n");
+
+return 0;
+}
